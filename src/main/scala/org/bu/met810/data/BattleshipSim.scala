e48c1b98c2c411039516c583ca8d2fb9b95621a2
Kenny <krlu91@gmail.com>
2019-01-16 20:25:59 -0500

    adding code for running battleshipsim in batches

--- src/main/scala/org/bu/met810/data/BattleshipSim.scala
+++ src/main/scala/org/bu/met810/data/BattleshipSim.scala
@@ -1,8 +1,8 @@
 package org.bu.met810.data
 
+import org.bu.met810.choose
 import org.bu.met810.models.PlayerModel
 import org.bu.met810.types.battleshipassets.{Board, Move, Player}
-import org.bu.met810.choose
 
 class BattleshipSim(initialBoard: Board,
                     val model1: PlayerModel[Board, Player, Move],
@@ -33,13 +33,13 @@ object BattleshipSim{
   def randomInitialization(width: Int, height: Int, numPieces: Int,
                            model1: PlayerModel[Board, Player, Move],
                            model2: PlayerModel[Board, Player, Move]): BattleshipSim = {
-    val p1 = initPlayer(width, height, numPieces)
-    val p2 = initPlayer(width, height, numPieces)
+    val p1 = initPlayer(width, height, numPieces, 1)
+    val p2 = initPlayer(width, height, numPieces, 2)
     val initialBoard = Board(p1, p2, width, height)
     new BattleshipSim(initialBoard, model1, model2)
   }
 
-  private def initPlayer(width: Int, height: Int, numPieces:Int): Player = {
+  private def initPlayer(width: Int, height: Int, numPieces:Int, id: Int): Player = {
     var openPositions = {for{
       x <- 0 until width
       y <- 0 until height
@@ -49,6 +49,24 @@ object BattleshipSim{
       openPositions = openPositions.filter(_ != pos)
       pos
     }
-    Player(piecePositions.toList)
+    Player(piecePositions.toList, id)
+  }
+
+  def runBatch(p1Model: PlayerModel[Board, Player, Move],
+               p2Model: PlayerModel[Board, Player, Move],
+               numTrials: Int = 1000, boardSize: Int = 4,
+               numPieces: Int): (Int, Int) = {
+    val start = System.currentTimeMillis()
+    val winners: Seq[Player] = {
+      for(_ <- 1 to numTrials) yield {
+        val sim = randomInitialization(boardSize, boardSize,numPieces, p1Model, p2Model)
+        sim.runFullGame()
+      }
+    }.flatten
+    val p1Wins = winners.filter(p => p.id == 1)
+    val p2Wins = winners.filter(p => p.id == 2)
+    val end = System.currentTimeMillis()
+    println(s"runtime: ${(end - start).toDouble/1000}")
+    (p1Wins.size, p2Wins.size)
   }
 }
